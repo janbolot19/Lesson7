517f1ddf560ff6f0ebd6468c0c47b8371219ff24
Janbolot <98080355+janbolot19@users.noreply.github.com>
2023-04-04 04:55:04 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const cards = document.getElementById('cards')
+const url = 'https://jsonplaceholder.typicode.com/posts'
+
+const getData = async (url) => {
+    await fetch(url)
+        .then(res =>  res.json())
+        .then(data => {
+            data.forEach(element => {
+                const div = document.createElement('div')
+                div.innerHTML = `
+                    <div class="card_block">
+                         <img src="news.png" alt="">
+                         <h2>${element.title}</h2>
+                         <p>${element.body}</p>
+                    </div>
+                `
+                cards.append(div)
+            })
+        })
+}
+
+getData(url)
